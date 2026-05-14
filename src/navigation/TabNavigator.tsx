07863c485432daafdd9de1e629678e9a5f39daf8
TopGSE <solomon.ekov@gmail.com>
2026-05-14 13:32:19 +0200

    icons toegevoegd aan de tabnavigator eerst was dit een if else om icons te tonen maar nu neem hij ze gewoon van een variabelen wat cleaner is

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,14 +1,36 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import SubjectsScreen from "../screens/Subjects";
 import TasksScreen from "../screens/Tasks";
 import ProfileScreen from "../screens/Profile";
 
 const Tab = createBottomTabNavigator();
+const tabIcons: Record<
+  string,
+  [keyof typeof Ionicons.glyphMap, keyof typeof Ionicons.glyphMap]
+> = {
+  Home: ["home", "home-outline"],
+  Vakken: ["book", "book-outline"],
+  Taken: ["checkmark-circle", "checkmark-circle-outline"],
+  Profiel: ["person", "person-outline"],
+};
 
 export default function TabNavigator({ setIsAuthenticated }: any) {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          const [active, inactive] = tabIcons[route.name] ?? [
+            "home",
+            "home-outline",
+          ];
+          const iconName = focused ? active : inactive;
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Vakken" component={SubjectsScreen} />
       <Tab.Screen name="Taken" component={TasksScreen} />
